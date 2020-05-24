2f04c07d394f8396647334417c503c11a2ab0e9a
Riley Shahar <riley.shahar@gmail.com>
2020-05-24 11:19:25 -0700

    Add nvim to setup brew formulae

--- setup.sh
+++ setup.sh
@@ -3,7 +3,10 @@ sudo xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew cask install dropbox google-chrome alfred telegram steam whatsapp iterm2 atom skim rstudio discord flux slack vlc java mactex
-brew install r trash git youtube-dl tree zsh fzf ack python3 pipenv black
+brew install r trash git youtube-dl tree zsh fzf ack python3 pipenv black nvim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 git config --global user.name "Riley Shahar"
 git config --global user.email "riley.shahar@gmail.com"
@@ -15,5 +18,7 @@ git config --global user.email "riley.shahar@gmail.com"
 # ln -sv ~/code/dotfiles/.zshrc ~/ 
 # ln -sv ~/code/dotfiles/.gitignore ~/ 
 # ln -sv ~/code/dotfiles/.gitconfig ~/ 
+# ln -sv ~/code/dotfiles/init.vim ~/
+# nvim plugin management
 
 sudo spctl --master-disable  # disable unidentified developer
