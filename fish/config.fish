fb6963fbc6cb66e29b6773fbf43eb6afc257de74
Riley Shahar <riley.shahar@gmail.com>
2020-12-15 02:03:05 -0800

    add nvim lsp

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 # configure path
-set paths_to_add /usr/local/opt/python@3.8 /bin/usr/local/opt/ruby/bin $HOME/.cargo/bin
+set paths_to_add /usr/local/opt/python@3.8 /bin/usr/local/opt/ruby/bin $HOME/.cargo/bin /usr/local/opt/llvm/bin/
 
 for path in $paths_to_add
         contains $path $fish_user_paths; or set -Ua fish_user_paths $path
@@ -76,6 +76,8 @@ set -x LANG "en_US.UTF-8"
 set -x FZF_DEFAULT_COMMAND "rg --files --hidden"
 
 set -x BAT_THEME "TwoDark"
+
+set -x CMAKE_EXPORT_COMPILE_COMMANDS true
 
 
 ### ABBREVIATIONS
