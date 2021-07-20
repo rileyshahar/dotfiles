46ec495b608255b8e4443ec7c2099ff136e7e5fe
Riley Shahar <riley.shahar@gmail.com>
2021-07-20 12:45:47 -0700

    [rofi] remove transparency
    It prevented us from adequately seeing the options, since rofi is
    generally on top of other windows.
    
    Also properly install rofi in the install script.

--- install.sh
+++ install.sh
@@ -135,6 +135,7 @@ ln -sv "$DOTFILES_DIR/cac" "$CONFIG_HOME" > /dev/null
 ln -sv "$DOTFILES_DIR/picom" "$CONFIG_HOME" > /dev/null
 ln -sv "$DOTFILES_DIR/mpv" "$CONFIG_HOME" > /dev/null
 ln -sv "$DOTFILES_DIR/fontconfig" "$CONFIG_HOME" > /dev/null
+ln -sv "$DOTFILES_DIR/rofi" "$CONFIG_HOME" > /dev/null
 ln -sv "$DOTFILES_DIR/discord/themes" "$CONFIG_HOME/BetterDiscord" > /dev/null
 ln -sv "$DOTFILES_DIR/nvim/init.lua" "$CONFIG_HOME/nvim" > /dev/null
 ln -sv "$DOTFILES_DIR/nvim/lua/" "$CONFIG_HOME/nvim" > /dev/null
