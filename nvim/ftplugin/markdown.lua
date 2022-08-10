65d51011e9228e926e903078767f1f7a49bb7d19
Riley Shahar <riley.shahar@gmail.com>
2022-08-10 02:41:08 -0700

    Update nvim-surround version

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -2,14 +2,27 @@
 -- automatically insert markdown links
 -- TODO: there's a buffer_setup that we really want here, but just isn't working for some reason
 require("nvim-surround").setup({
-	delimiters = {
-		pairs = {
-			["l"] = function()
+	surrounds = {
+		["l"] = {
+			add = function()
+				local clipboard = string.gmatch(vim.fn.getreg("+"), "[^\n]+")()
 				return {
-					"[",
-					"](" .. string.gmatch(vim.fn.getreg("+"), "[^\n]+")() .. ")",
+					{ "[" },
+					{ "](" .. clipboard .. ")" },
 				}
 			end,
+			find = "%b[]%b()",
+			delete = "^(%[)().-(%]%b())()$",
+			change = {
+				target = "^()()%b[]%((.-)()%)$",
+				replacement = function()
+					local clipboard = vim.fn.getreg("+"):gsub("\n", "")
+					return {
+						{ "" },
+						{ clipboard },
+					}
+				end,
+			},
 		},
 	},
 })
