712e6b219933a1e4a0d827e74a633db0c8b3355e
Riley Shahar <riley.shahar@gmail.com>
2022-10-19 13:46:39 -0700

    Use newer cmp-lsp function

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -63,8 +63,7 @@ local on_attach = function(client, bufnr)
 	end
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 nvim_lsp.util.default_config = vim.tbl_extend("force", nvim_lsp.util.default_config, {
 	on_attach = on_attach,
