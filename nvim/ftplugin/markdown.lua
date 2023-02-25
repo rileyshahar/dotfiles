214dceec7ea295da1a2ce088db5765c3827502d6
Riley Shahar <riley.shahar@gmail.com>
2023-02-25 14:35:38 -0800

    [nvim] asynchronously make markdown

--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,6 +1,8 @@
 -- from https://github.com/kylechui/nvim-surround/discussions/53
 -- automatically insert markdown links
 -- TODO: there's a buffer_setup that we really want here, but just isn't working for some reason
+vim.bo.makeprg = "pandoc -d default % -o /tmp/%<.pdf"
+
 require("nvim-surround").setup({
 	surrounds = {
 		["l"] = {
