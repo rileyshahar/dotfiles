f5bbc67990e10095fc05776f25e4424b9db3597d
Riley Shahar <riley.shahar@gmail.com>
2023-03-08 21:33:08 -0800

    [snippets] add todo with no colon
    This is especially useful for easy highlights for homework.

--- nvim/luasnippets/all.lua
+++ nvim/luasnippets/all.lua
@@ -2,7 +2,8 @@
 local ls = require("luasnip")
 
 return {
-	s({ trig = "td", name = "TODO", dscr = "A simple TODO." }, t("TODO: ")),
+	s({ trig = "td", name = "TODO", dscr = "A simple TODO." }, t("TODO")),
+	s({ trig = "td:", name = "TODO:", dscr = "TODO with a message" }, t("TODO: ")),
 	s({ trig = "atd", name = "assigned TODO", dscr = "An assigned TODO." }, fmt("TODO ({}): ", i(1))),
 	s({ trig = "dm", name = "date-month", dscr = "The date and the month." }, p(os.date, "%d-%m")),
 	s({ trig = "md", name = "month/date", dscr = "The month and the date." }, p(os.date, "%m/%d")),
