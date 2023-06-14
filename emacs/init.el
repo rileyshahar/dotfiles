fb27d651ad54f28101129c46b4b32f21a72fad3f
Riley Shahar <riley.shahar@gmail.com>
2023-06-13 21:00:35 -0400

    [emacs] improve config

--- emacs/init.el
+++ emacs/init.el
@@ -1,26 +1,38 @@
-(require 'package)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                        ("org" . "https://orgmode.org/elpa/")
-                        ("elpa" . "https://elpa.gnu.org/packages/")))
+(straight-use-package 'use-package)
 
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(require 'use-package)
-; always ensure packages are loaded
-(setq use-package-always-ensure t)
+(use-package straight
+  :custom
+  (straight-use-package-by-default t))
 
 (use-package general
   :config
   (general-evil-setup t))
 
+(general-auto-unbind-keys)
+
 (general-create-definer rs/leader-def
+  :keymaps 'normal
   :prefix ",")
 
 (general-create-definer rs/local-leader-def
+  :keymaps 'normal
   :prefix "\\")
+
+(rs/leader-def
+  "bp" '(previous-buffer :wk "previous buffer"))
 
 (use-package evil
   :init
@@ -29,7 +41,6 @@
   (setq evil-want-C-u-scroll t)
   :config
   (evil-mode)
-  (define-key evil-insert-state-map (kbd "<C-return>") 'evil-normal-state)
   (evil-set-undo-system 'undo-redo))
 
 (use-package evil-collection
@@ -43,6 +54,10 @@
   :config
   (global-evil-surround-mode 1))
 
+(general-define-key
+ :keymaps 'evil-insert-state-map
+ (general-chord "jk") '(evil-normal-state :wk "exit insert mode"))
+
 (use-package key-chord
   :config
   (key-chord-mode 1))
@@ -50,8 +65,9 @@
 (setq key-chord-two-keys-delay 0.5)
 (setq key-chord-safety-interval-backward 0.0) ; always interpret a key as starting a chord
 (setq key-chord-safety-interval-forward 0.0)  ; always immediately execute a chord which completes
-(key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
-(key-chord-mode 1)
+
+(use-package which-key)
+(which-key-mode)
 
 (defun rs/org-mode-setup ()
   (org-indent-mode))
@@ -59,16 +75,26 @@
 (use-package org
   :hook (org-mode . rs/org-mode-setup)
   :config
-  (setq org-ellipsis " ▾")
+  (setq org-ellipsis " ▾") ; sets symbol for collapsed headers
   (setq org-return-follows-link t))
+
+(general-def org-mode-map
+ "RET" '(org-return :wk "return"))
 
 (use-package org-auto-tangle
   :defer t
   :hook (org-mode . org-auto-tangle-mode))
 
-; auto continue lists
 (use-package org-autolist
   :hook (org-mode . org-autolist-mode))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode))
+
+(use-package toc-org
+    :commands toc-org-enable
+    :hook (org-mode . toc-org-enable))
 
 ; completion
 (use-package vertico
@@ -100,18 +126,18 @@
 ; dependency: icons
 (use-package all-the-icons)
 
-; prettier modeline
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode))
-
 (set-face-attribute 'default nil :font "MesloLGS Nerd Font 11")
+
+(set-face-attribute 'font-lock-comment-face nil
+  :slant 'italic)
 
 (global-display-line-numbers-mode 1)
 
 (setq visible-bell t)                                   ; set visual instead of audio bell
 (doom-themes-visual-bell-config)
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit) ; Make ESC quit prompts
+
+(use-package bug-hunter)
 
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (when (file-exists-p custom-file)
