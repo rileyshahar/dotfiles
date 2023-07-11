0507339e27a8bad5cc8941588493f7398c4e06fe
Riley Shahar <riley.shahar@gmail.com>
2023-07-11 12:03:14 -0400

    [nvim] startuptime improvements
    The big one is lazy-loading neorg.

--- nvim/lua/plugins/filetype.lua
+++ nvim/lua/plugins/filetype.lua
@@ -1,113 +1,118 @@
 return {
-  {
-    "whonore/Coqtail",
-    ft = "coq",
-    config = function()
-      vim.g.coqtail_map_prefix = "<localleader>"
-    end,
-    keys = {
-      {
-        "]c",
-        "<cmd>CoqNext<cr><cmd>CoqJumpToEnd<cr>",
-        desc = "coq: step forward",
-      },
-      {
-        "[c",
-        "<cmd>CoqUndo<cr><cmd>CoqJumpToEnd<cr>",
-        desc = "coq: step back",
-      },
-      { "<localleader>/", ":Coq Search ", desc = "coq search" }, -- have to use : to not need to close the cmd
-    },
-  },
-  -- {
-  -- 	"lervag/vimtex",
-  -- 	ft = "tex"
-  -- },
-  {
-    "dag/vim-fish",
-    ft = "fish"
-  },
-  {
-    "isovector/cornelis",
-    ft = "agda"
-  },
-  "kmonad/kmonad-vim",
-  {
-    "akinsho/toggleterm.nvim",
-    config = true,
-    keys = {
-      {
-        leaders.terminal .. leaders.terminal,
-        "<cmd>ToggleTerm<cr>",
-        desc = "toggle terminal",
-        mode = { "n", "t", "i", "v" },
-      },
-      {
-        leaders.terminal .. "s",
-        "<cmd>TermSelect<cr>",
-        desc = "select terminal",
-        mode = { "n", "t" },
-      },
-      {
-        leaders.terminal .. "n",
-        "<cmd>ToggleTermSetName<cr>",
-        desc = "name terminal",
-        mode = { "n", "t" },
-      },
-      {
-        leaders.terminal .. "<cr>",
-        "<cmd>ToggleTermSendCurrentLine<cr>",
-        desc = "send current line to terminal",
-        mode = { "n", "i" },
-      },
-      {
-        leaders.terminal .. "<cr>",
-        "<cmd>ToggleTermSendVisualSelection<cr>",
-        desc = "send visual seleciton to terminal",
-        mode = "x",
-      },
-      {
-        "<esc>",
-        "<c-\\><c-n>",
-        desc = "exit insert mode",
-        mode = "t",
-      },
-      {
-        "jk",
-        "<c-\\><c-n>",
-        desc = "exit insert mode",
-        mode = "t",
-      },
-      {
-        "<c-h>",
-        "<cmd>wincmd h<cR>",
-        desc = "move to left window",
-        mode = "t",
-      },
-      {
-        "<c-j>",
-        "<cmd>wincmd j<cR>",
-        desc = "move to right window",
-        mode = "t",
-      },
-      {
-        "<c-k>",
-        "<cmd>wincmd k<cR>",
-        desc = "move to above window",
-        mode = "t",
-      },
-      {
-        "<c-l>",
-        "<cmd>wincmd l<cR>",
-        desc = "move to below window",
-        mode = "t",
-      },
-      {
-        "<c-w>",
-        "<c-\\><c-n><c-w>",
-        desc = "window",
-        mode = "t",
-      },
-    }
-  }
+	{
+		"whonore/Coqtail",
+		ft = "coq",
+		config = function()
+			vim.g.coqtail_map_prefix = "<localleader>"
+		end,
+		keys = {
+			{
+				"]c",
+				"<cmd>CoqNext<cr><cmd>CoqJumpToEnd<cr>",
+				desc = "coq: step forward",
+			},
+			{
+				"[c",
+				"<cmd>CoqUndo<cr><cmd>CoqJumpToEnd<cr>",
+				desc = "coq: step back",
+			},
+			{ "<localleader>/", ":Coq Search ", desc = "coq search" }, -- have to use : to not need to close the cmd
+		},
+	},
+
+	-- {
+	-- 	"lervag/vimtex",
+	-- 	ft = "tex"
+	-- },
+
+	{
+		"dag/vim-fish",
+		ft = "fish"
+	},
+
+	{
+		"isovector/cornelis",
+		ft = "agda"
+	},
+
+	"kmonad/kmonad-vim",
+
+	{
+		"akinsho/toggleterm.nvim",
+		config = true,
+		keys = {
+			{
+				leaders.terminal .. leaders.terminal,
+				"<cmd>ToggleTerm<cr>",
+				desc = "toggle terminal",
+				mode = { "n", "t", "i", "v" },
+			},
+			{
+				leaders.terminal .. "s",
+				"<cmd>TermSelect<cr>",
+				desc = "select terminal",
+				mode = { "n", "t" },
+			},
+			{
+				leaders.terminal .. "n",
+				"<cmd>ToggleTermSetName<cr>",
+				desc = "name terminal",
+				mode = { "n", "t" },
+			},
+			{
+				leaders.terminal .. "<cr>",
+				"<cmd>ToggleTermSendCurrentLine<cr>",
+				desc = "send current line to terminal",
+				mode = { "n", "i" },
+			},
+			{
+				leaders.terminal .. "<cr>",
+				"<cmd>ToggleTermSendVisualSelection<cr>",
+				desc = "send visual seleciton to terminal",
+				mode = "x",
+			},
+			{
+				"<esc>",
+				"<c-\\><c-n>",
+				desc = "exit insert mode",
+				mode = "t",
+			},
+			{
+				"jk",
+				"<c-\\><c-n>",
+				desc = "exit insert mode",
+				mode = "t",
+			},
+			{
+				"<c-h>",
+				"<cmd>wincmd h<cR>",
+				desc = "move to left window",
+				mode = "t",
+			},
+			{
+				"<c-j>",
+				"<cmd>wincmd j<cR>",
+				desc = "move to right window",
+				mode = "t",
+			},
+			{
+				"<c-k>",
+				"<cmd>wincmd k<cR>",
+				desc = "move to above window",
+				mode = "t",
+			},
+			{
+				"<c-l>",
+				"<cmd>wincmd l<cR>",
+				desc = "move to below window",
+				mode = "t",
+			},
+			{
+				"<c-w>",
+				"<c-\\><c-n><c-w>",
+				desc = "window",
+				mode = "t",
+			},
+		}
+	}
 }
--- nvim/lua/plugins/notes.lua
+++ nvim/lua/plugins/notes.lua
@@ -1,59 +1,60 @@
 return {
-	{
-		"nvim-neorg/neorg",
-		dev = true,
-		build = ":Neorg sync-parsers",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-neorg/neorg-telescope",
-		},
-		opts = {
-			load = {
-				["core.defaults"] = {},
-				["core.export"] = {},
-				["core.export.markdown"] = {},
-				["core.highlights"] = {
-					config = {
-						dim = {
-							tags = {
-								ranged_verbatim = {},
-							},
-						},
-					},
-				},
-				["core.dirman"] = {
-					config = {
-						workspaces = {
-							math = "~/notes/math",
-						},
-						index = "index.norg", -- The name of the main (root) .norg file
-						use_popup = false,
-					},
-				},
-				["core.completion"] = {
-					config = {
-						engine = "nvim-cmp",
-					},
-				},
-				["core.concealer"] = {},
-				-- TODO: descriptions
-				["core.keybinds"] = {
-					config = {
-						hook = function(keybinds)
-							keybinds.map("norg", "n", "<localleader>t", "<cmd>Neorg tangle current-file<cr>")
-							keybinds.map("norg", "n", "<localleader>,", "<cmd>Telescope neorg search_headings<cr>")
-							keybinds.map(
-								"norg",
-								"n",
-								"<C-]>",
-								"<cmd>Neorg keybind all core.looking-glass.magnify-code-block<cr>"
-							)
-						end,
-					},
-				},
-				["core.integrations.telescope"] = {},
-			},
-		},
-	},
+  {
+    "nvim-neorg/neorg",
+    dev = true,
+    build = ":Neorg sync-parsers",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-neorg/neorg-telescope",
+    },
+    ft = "norg",
+    opts = {
+      load = {
+        ["core.defaults"] = {},
+        ["core.export"] = {},
+        ["core.export.markdown"] = {},
+        ["core.highlights"] = {
+          config = {
+            dim = {
+              tags = {
+                ranged_verbatim = {},
+              },
+            },
+          },
+        },
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              math = "~/notes/math",
+            },
+            index = "index.norg", -- The name of the main (root) .norg file
+            use_popup = false,
+          },
+        },
+        ["core.completion"] = {
+          config = {
+            engine = "nvim-cmp",
+          },
+        },
+        ["core.concealer"] = {},
+        -- TODO: descriptions
+        ["core.keybinds"] = {
+          config = {
+            hook = function(keybinds)
+              keybinds.map("norg", "n", "<localleader>t", "<cmd>Neorg tangle current-file<cr>")
+              keybinds.map("norg", "n", "<localleader>,", "<cmd>Telescope neorg search_headings<cr>")
+              keybinds.map(
+                "norg",
+                "n",
+                "<C-]>",
+                "<cmd>Neorg keybind all core.looking-glass.magnify-code-block<cr>"
+              )
+            end,
+          },
+        },
+        ["core.integrations.telescope"] = {},
+      },
+    },
+  },
 }
