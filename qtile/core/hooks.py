6de1f8f4205bd250b54c3166cd196a9dd8a45000
Riley Shahar <riley@rileyshahar.com>
2023-10-17 17:07:24 -0700

    [qtile] use new opacity api
    Unfortunately, this doesn't work on wayland anymore.

--- qtile/core/hooks.py
+++ qtile/core/hooks.py
@@ -4,6 +4,7 @@ import subprocess
 
 from libqtile import hook
 from libqtile.backend.base import Window
+from libqtile.utils import send_notification
 
 from .settings import DAEMONS, TRANSPARENT_CLASSES
 
@@ -17,16 +18,17 @@ def start_once() -> None:
 
 def _should_float(window: Window) -> bool:
     """Check whether a window should float."""
-    return window.name == "__float" or "zettlemath" in window.name
+    return window.name == "__float"
 
 
 @hook.subscribe.client_new  # type: ignore
 def new_client(window: Window) -> None:
     """Set transparency and floats for the correct windows."""
     if any(any(app in c for app in TRANSPARENT_CLASSES) for c in window.get_wm_class()):
-        window.cmd_opacity(0.8)
+        # TODO: broken by new qtile update
+        window.set_opacity(0.8)
 
     if _should_float(window):
-        window.cmd_enable_floating()
-        window.cmd_set_size_floating(950, 650)
-        window.cmd_opacity(1.0)
+        window.enable_floating()
+        window.set_size_floating(950, 650)
+        window.set_opacity(1.0)
