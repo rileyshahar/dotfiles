18b582a10e0ef86e39d3ebb2268363d4dd711e0c
Riley Shahar <riley@rileyshahar.com>
2023-12-19 18:03:18 -0800

    [nvim] add neovide font size commands

--- nvim/lua/neovide.lua
+++ nvim/lua/neovide.lua
@@ -1,5 +1,39 @@
 vim.g.neovide_transparency = 0.8
 vim.g.neovide_cursor_animation_length = 0.0
 vim.g.neovide_cursor_trail_size = 0.0
-vim.o.guifont = "JetBrainsMono Nerd Font:h10" -- text below applies for VimScript
+-- vim.o.guifont = "JetBrainsMono Nerd Font:h10" -- text below applies for VimScript
 vim.g.neovide_scroll_animation_length = 0.0
+
+vim.g.gui_font_default_size = 10
+vim.g.gui_font_size = vim.g.gui_font_default_size
+vim.g.gui_font_face = "JetBrainsMono Nerd Font"
+
+RefreshGuiFont = function()
+	vim.opt.guifont = string.format("%s:h%s", vim.g.gui_font_face, vim.g.gui_font_size)
+end
+
+ResizeGuiFont = function(delta)
+	vim.g.gui_font_size = vim.g.gui_font_size + delta
+	RefreshGuiFont()
+end
+
+ResetGuiFont = function()
+	vim.g.gui_font_size = vim.g.gui_font_default_size
+	RefreshGuiFont()
+end
+
+-- Call function on startup to set default value
+ResetGuiFont()
+
+-- Keymaps
+map("<C-+>", function()
+	ResizeGuiFont(1)
+end, "increase font size", { "n", "i", "t" })
+
+map("<C-->", function()
+	ResizeGuiFont(-1)
+end, "decrease font size", { "n", "i", "t" })
+
+map("<C-=>", function()
+	ResetGuiFont()
+end, "reset font size", { "n", "i", "t" })
